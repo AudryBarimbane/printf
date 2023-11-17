55a259dbc55cb4a3fee8f6bdf447bf856ef32e19
Audry Barimbane <drybary@gmail.com>
2023-11-17 06:51:33 -0800

    update

---
+++ _printf.c
@@ -0,0 +1,52 @@
+#include "main.h"
+
+/**
+ * _printf -  produces output according to a format
+ * @format: a character string. The format string is composed of zero or more directives.
+ *
+ * Return:  number of characters printed (excluding the null byte used to end output to strings)
+ */
+
+int _printf(const char *format, ...)
+{
+	va_list ap;
+	int n = 0;
+
+	va_start(ap, format);
+
+	while (*format != '\0')
+	{
+
+	if (*format == "%")
+		format++;
+		if (*format == "%")
+			_putchar("%");
+			n++;
+		else if (*format == "c")
+		{
+			int c1 = va_arg(ap, int);
+			_putchar(c1);
+			n++;
+		}
+		else if (*format == "str")
+			char *str = va_arg(ap, char *);
+			whhile (*str)
+			{
+				_putchar(*str);
+				str++;
+				n++;
+			}
+		else 
+		{
+			_putchar(*format);
+			n++;
+		}
+		format++;
+	}
+	va_end(ap);
+	return (n);
+
+
+
+}
+
